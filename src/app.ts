548d2739bd42ce233c8e496eac5080ebe0d02148
Barkat-Ullah <barkatullah585464@gmail.com>
2026-08-10 18:27:41 +0600

    cors : fix cors issue and replace the production url

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,8 @@ export const corsOptions = {
   origin: [
     "http://localhost:3001",
     "http://localhost:3000",
+    "https://league-core.vercel.app",
+    "https://platform.leaguecore.barkatullah.dev"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
